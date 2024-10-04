ab25a2b1b973c72732c2fbcb226a6d98e8fa2f61
ALROB <alexanderroberts@acresecurity.com>
2024-10-04 13:16:32 -0400

    put stuff back

---
+++ helloWorld/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CommandLine;
+using System.IO;
+
+namespace helloWorld
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('f', "folder", Required = true, HelpText = "Path to the folder whose contents should be printed")]
+            public string Folder { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            string folder = "";
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    folder = o.Folder;
+                });
+            
+            foreach(string filePath in Directory.GetFiles(folder))
+            {
+                Console.WriteLine("printing from: " + filePath);
+                string fileContent = File.ReadAllText(filePath);
+                Console.WriteLine(fileContent);
+            }
+            
+        }
+    }
+}
